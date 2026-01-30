39b58d9659e3b539a5122b292a2d03af123acc6d
Zeeshan Ali <zeeshanali3297624@gmail.com>
2026-01-31 04:08:14 +0500

    PWA-changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -169,12 +169,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
-        <StoreProvider>
+        {/* <StoreProvider>
           <ClientLayout>
             {children}
             <WhatsAppButton />
           </ClientLayout>
-        </StoreProvider>
+        </StoreProvider> */}
+        <ComingSoonPage />
       </body>
     </html>
   );
